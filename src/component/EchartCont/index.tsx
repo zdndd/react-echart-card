5a1c2ffc57edf864c33976e4dd4ed608260cc417
zdndd <dongningdella@gmail.com>
2023-12-20 10:05:04 +0800

    echart init

--- src/component/EchartCont/index.tsx
+++ src/component/EchartCont/index.tsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import "./echartcont.css";
+import * as echarts from "echarts/core";
 
-const EchartCont = () => {
-  return <div className="wrap"></div>;
-};
+class EchartCont extends Component {
+  componentDidMount() {
+    console.log(document.getElementById("main"));
+    var myChart = echarts.init(document.getElementById("main"));
+  }
+  render() {
+    return (
+      <div className="wrap">
+        <div id="main">111</div>
+      </div>
+    );
+  }
+}
 
 export default EchartCont;
